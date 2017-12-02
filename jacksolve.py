c9b2f27812a8070bfb8fb432097f46b481ebce29
Neal Derman <neal@travelground.com>
2017-12-02 14:31:15 +0200

    convert hands to useful format and sort by card value

--- jacksolve.py
+++ jacksolve.py
@@ -1,4 +1,31 @@
 import urllib.request, json
+import operator
 with urllib.request.urlopen("https://s3-eu-west-1.amazonaws.com/yoco-testing/tests.json") as url:
     data = json.loads(url.read().decode())
-    print(data)
+
+
+tens = {'J','Q','K'}
+
+# print(data)
+
+def checkFaceCard(cardValue):
+	if cardValue in tens:
+		return 10
+	elif cardValue == 'A':
+		return 11
+	else:
+		return int(cardValue)
+
+def sortCards(hand):
+	cardlist = {}
+	for idx, card in enumerate(hand):
+		cardlist[card[-1:]] = checkFaceCard(card[0:-1])
+		cardListSorted = sorted(cardlist.items(), key=operator.itemgetter(1), reverse = True)
+		return cardListSorted
+
+
+# loop through json results
+for result in data:
+	for key in result:
+		playerAHand = sortCards(result['playerA'])
+		playerBhand = sortCards(result['playerB'])
