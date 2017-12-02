e28b279d460f5ebb774ab40b504925b986c7b03e
Neal Derman <neal@travelground.com>
2017-12-02 15:25:26 +0200

    simple sum and 21 cases done. Now the fun ones

--- jacksolve.py
+++ jacksolve.py
@@ -24,14 +24,49 @@ def sortCards(hand):
 	return cardListSorted
 
 def compareHands(playerAHand, playerBHand):
-	if (min(len(playerAHand),len(playerBHand))) == 1:
-		print(playerAHand)
-		print(playerBHand)
-		print('****')
+	print(playerAHand)
+	print(playerBHand)
+	sumA = 0
+	sumB = 0
+
+	for card in playerAHand:
+		sumA = sumA + card[1]
+	for card in playerBHand:
+		sumB = sumB + card[1]
+	print (sumA)
+	print (sumB)
+	if sumA > 21: #if A greater than 21 A loses
+		return 0
+	elif sumB > 21: #if B > 21 A wins
+		return 1
+	elif sumA > sumB: #if A>B A wins
+		return 1
+	elif sumB == sumA: #if equal we're not sure
+		return 2
+	else:			#otherwise B wins
+		return 0
+
+
+	# loopMax = max(len(playerAHand),len(playerBHand))
+	# for i in range(loopMax):
+	# 	print (playerAHand[min(len(playerAHand)-1,i)][1])
+	# 	print (playerBHand[min(len(playerBHand)-1,i)][1])
+	# 	if playerAHand[min(len(playerAHand)-1,i)] > playerBHand[min(len(playerBHand)-1,i)]:
+	# 		return 1
+	# return 0
 
 
 # loop through json results
 for result in data:
 	playerAHand = sortCards(result['playerA'])
 	playerBHand = sortCards(result['playerB'])
-	compareHands(playerAHand,playerBHand)
+	print(compareHands(playerAHand, playerBHand))
+	print('****')
+
+
+
+
+
+
+
+
